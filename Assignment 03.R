f31657f70168b52972bb7f42cb93d7e51ef90397
Nik328 <99050689+Nik328@users.noreply.github.com>
2022-03-11 08:00:11 -0500

    Add files via upload

---
+++ Assignment 03.R
@@ -0,0 +1,16 @@
+
+header <- read.table("yellow_tripdata_2021-03.csv", header = TRUE,
+                     sep=",", nrow = 1)
+DF <- fread("yellow_tripdata_2021-03.csv",
+            skip=1, sep=",",header=FALSE,
+            data.table=FALSE)
+setnames(DF, colnames(header))
+rm(header)
+
+
+DF2<-DF[which(DF$trip_distance > 10 |
+                DF$trip_distance < 0 ),]
+
+avg_dist <- mean(DF2$trip_distance)
+avg_cost <- mean(DF2$total_amount)
+cat("The average distance traveled by a person in a taxi cab in NYC in March of 2021 is" ,avg_dist, "miles while the average total cost for these trips was", avg_cost)
